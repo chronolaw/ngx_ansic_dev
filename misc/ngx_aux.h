9f92bda4a0cf4290dd699b5b48a4d3e333636246
chronolaw <chrono_cpp@me.com>
2018-09-04 18:00:00 +0800

    code optimization

--- misc/ngx_aux.h
+++ misc/ngx_aux.h
@@ -3,22 +3,29 @@
 #ifndef _NGX_AUX_H_INCLUDED_
 #define _NGX_AUX_H_INCLUDED_
 
+// ngx_array_t aux
+
+#define ngx_array_reset(array)                                                \
+    (array)->nelts = 0;
+
+#define ngx_array_empty(array)                                                \
+    ((array)->nelts == 0)
+
+// ngx_list_t aux
+
 #define ngx_list_each(elt, list)                                              \
     do {                                                                      \
-        void             *_end;                                           \
-        ngx_list_part_t  *_part = &((list)->part);                    \
-                                                                          \
-        do {                                                              \
-            elt = _part->elts;                                   \
-                                                                          \
-            for (_end = (elt + _part->nelts); elt != _end; elt++) {       \
+        void             *_end;                                               \
+        ngx_list_part_t  *_part;                                              \
+                                                                              \
+        for (_part = &((list)->part); _part; _part = _part->next) {           \
+            elt = _part->elts;                                                \
+                                                                              \
+            for (_end = (elt + _part->nelts); elt != _end; elt++) {           \
 
 #define ngx_list_loop                                                         \
-            }                                                             \
-                                                                          \
-            _part = _part->next;                                          \
-                                                                          \
-        } while (_part != NULL);                                          \
+            }                                                                 \
+        }                                                                     \
     } while (0)
 
 #endif  // _NGX_AUX_H_INCLUDED_
