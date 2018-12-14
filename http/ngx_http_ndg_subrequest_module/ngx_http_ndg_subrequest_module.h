6e985bc51bf8ec36201e1ad99bbab34888cda419
chronolaw <chrono_cpp@me.com>
2018-12-14 09:48:30 +0800

    ngx_http_ndg_subrequest_module.h

--- http/ngx_http_ndg_subrequest_module/ngx_http_ndg_subrequest_module.h
+++ http/ngx_http_ndg_subrequest_module/ngx_http_ndg_subrequest_module.h
@@ -7,6 +7,10 @@
 #include <ngx_http.h>
 #include <assert.h>
 
+#if nginx_version < 1013010
+    #error "need latest nginx!"
+#endif
+
 typedef struct {
     ngx_str_t uri;
 } ngx_http_ndg_subrequest_loc_conf_t;
